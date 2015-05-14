22e6297935e09e5916db280434430ea05c36317f
desarrollocorrales <desarrollo@loscorrales.com.mx>
2015-05-14 12:25:35 -0500

    Inicia el proyecto
    Se agrega el proyecto e inicia el desarrollo

---
+++ SelectivasEnSucursales/GUIs/FrmPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SelectivasEnSucursales.GUIs
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Buscar();
+        }
+        private void Buscar()
+        {
+            string sClave = txbClave.Text.ToUpper();
+        }
+
+    }
+}
---
+++ SelectivasEnSucursales/Modelos/Articulo.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SelectivasEnSucursales.Modelos
+{
+    public class Articulo
+    {
+        public int Articulo_ID    { set; get; }
+        public string Nombre      { set; get; }
+        public string Clave       { set; get; }
+        public decimal Existencia { set; get; }
+    }
+}
